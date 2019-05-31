429ed7bf264e460c44c35a92fc83c2006b1a9fe3
Vera Nikalaikina <nikolajkina@gmail.com>
2019-05-31 17:15:40 +0300

    Transition

--- src/main/scala/ComonadUi.scala
+++ src/main/scala/ComonadUi.scala
@@ -1,7 +1,10 @@
-import cats.Comonad
 import cats.data.State
 import cats.syntax.coflatMap._
 import cats.syntax.comonad._
+import cats.syntax.functor._
+import cats.{Comonad, Functor, Monad}
+
+import scala.language.higherKinds
 
 object ComonadUi extends App {
   import State._
@@ -34,7 +37,7 @@ object Store {
     }
   }
 
-  implicit def pairing[S] = new Pairing[State[S, ?], Store[S, ?]] {
+  implicit def pairing[S]: Pairing[State[S, ?], Store[S, ?]] = new Pairing[State[S, ?], Store[S, ?]] {
     override def pair[A, B, C]: (A => B => C) => State[S, A] => Store[S, B] => C = { f => state => store =>
       val (next, a) = state.run(store.state).value
       f(a)(store.show(next))
@@ -45,4 +48,38 @@ object Store {
 trait Pairing[M[_], W[_]] {
   def pair[A, B, C]: (A => B => C) => M[A] => W[B] => C
   def select[A, B]: M[B] => W[W[A]] => W[A] = pair(_ => identity)
+}
+
+trait Transition[W[_], A] {
+  def runTransition[R]: W[A => R] => R
+}
+
+object Transition {
+
+  def extend[W[_]: Comonad : Functor, A, B]: (W[A] => B) => W[A] => W[B] = { f => _.coflatten.map(f) }
+
+  implicit def functor[W[_]: Functor]: Functor[Transition[W, ?]] = new Functor[Transition[W, ?]] {
+    override def map[A, B](fa: Transition[W, A])(f: A => B): Transition[W, B] = {
+      new Transition[W, B] {
+        def runTransition[R]: W[B => R] => R = { wbr => fa.runTransition(wbr.map(f andThen _)) }
+      }
+    }
+  }
+
+  implicit def monad[W[_]: Comonad: Functor, R]: Monad[Transition[W, ?]] = new Monad[Transition[W, ?]] {
+    override def flatMap[A, B](fa: Transition[W, A])(f: A => Transition[W, B]): Transition[W, B] = new Transition[W, B] {
+      override def runTransition[R]: W[B => R] => R = { wbr: W[B => R] =>
+        val x: W[B => R] => A => R = { wa: W[B => R] => a: A => f(a).runTransition(wa) }
+        fa.runTransition(wbr.coflatMap(x))
+      }
+    }
+
+
+    override def pure[A](x: A): Transition[W, A] = new Transition[W, A] {
+      override def runTransition[R]: W[A => R] => R = _.extract(x)
+    }
+
+    override def tailRecM[A, B](a: A)(f: A => Transition[W, Either[A, B]]): Transition[W, B] = ???
+  }
+
 }
